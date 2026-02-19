fe9cc6e58b52c7328c61f9df4e459179dfbfa257
Chinthala Durga Prasad <durgaprasadch.in@gmail.com>
2026-02-19 18:25:44 +0530

    Added employee salary calculator assessment

--- assessments/employeesalarycalculator/EmployeeSalaryCalculator.java
+++ assessments/employeesalarycalculator/EmployeeSalaryCalculator.java
@@ -1,5 +1,56 @@
 package com.demo.employeesalarycalculator;
 
 public class EmployeeSalaryCalculator {
-
+	private double baseSalary;
+	private double bonus;
+	private double taxPercent;
+	
+	public EmployeeSalaryCalculator(double baseSalary,double bonus,double taxPercent)
+	{
+		this.baseSalary = baseSalary;
+		this.bonus = bonus;
+		this.taxPercent = taxPercent;
+	}
+	public boolean validateSalary()
+	{
+		if(this.getBaseSalary()<=0)
+			throw new IllegalArgumentException();
+		if(this.getTaxPercent()<0 || this.getTaxPercent()>100)
+			throw new IllegalArgumentException();
+		return true;
+	}
+	public double calculateHRA() {
+		return  this.getBaseSalary() * 0.2;
+	}
+	public double calculateDA()
+	{
+		return this.getBaseSalary() * 0.1;
+	}
+	public double calculateGrossSalary()
+	{
+		return this.getBaseSalary()+this.calculateDA()+ this.calculateHRA()+ this.getBonus();
+	}
+	public double calculateNetSalary()
+	{
+		return (this.calculateGrossSalary() - ((this.getTaxPercent()/100)*this.calculateGrossSalary())); 
+	}
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+	public double getBonus() {
+		return bonus;
+	}
+	public void setBonus(double bonus) {
+		this.bonus = bonus;
+	}
+	public double getTaxPercent() {
+		return taxPercent;
+	}
+	public void setTaxPercent(double taxPercent) {
+		this.taxPercent = taxPercent;
+	}
+	
 }
--- assessments/employeesalarycalculator/EmployeeTestCalculator.java
+++ assessments/employeesalarycalculator/EmployeeTestCalculator.java
@@ -1,5 +1,28 @@
 package com.demo.employeesalarycalculator;
 
-public class EmployeeTestCalculator {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.*;
+
+
+public class EmployeeTestCalculator {
+	static EmployeeSalaryCalculator esc;
+	@Test
+	public void testSalary()
+	{
+		esc = new EmployeeSalaryCalculator(20000,5000,10);
+		assertTrue(esc.validateSalary());
+		assertEquals(2000.0,esc.calculateDA());
+		assertEquals(4000.0d,esc.calculateHRA());
+		assertEquals(31000.0d,esc.calculateGrossSalary());
+		assertEquals(27900.0d,esc.calculateNetSalary());
+	}
+	
+	@Test
+	public void testThrows()
+	{
+		esc = new EmployeeSalaryCalculator(0, 0, 100);
+		assertThrows(IllegalArgumentException.class,() -> esc.validateSalary());
+		assertThrows(IllegalArgumentException.class,() -> esc.validateSalary());
+	}
 }
