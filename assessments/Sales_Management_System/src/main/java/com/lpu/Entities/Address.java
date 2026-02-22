d935dbccd5e81e8a77188008a64c48c3a5aa3a81
Durga Prasad <119026227+durgaaprasadch@users.noreply.github.com>
2026-02-23 00:51:02 +0530

    Refactor Address entity methods for readability
    Reformatted getter and setter methods for better readability.

--- assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Address.java
+++ assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Address.java
@@ -9,14 +9,34 @@ public class Address {
  private Long id;
  private String street, city, state, zipCode;
 
- public Long getId(){return id;}
- public void setId(Long id){this.id=id;}
- public String getStreet(){return street;}
- public void setStreet(String street){this.street=street;}
- public String getCity(){return city;}
- public void setCity(String city){this.city=city;}
- public String getState(){return state;}
- public void setState(String state){this.state=state;}
- public String getZipCode(){return zipCode;}
- public void setZipCode(String zipCode){this.zipCode=zipCode;}
+ public Long getId(){
+  return id;
+ }
+ public void setId(Long id){
+  this.id=id;
+ }
+ public String getStreet(){
+  return street;
+ }
+ public void setStreet(String street){
+  this.street=street;
+ }
+ public String getCity(){
+  return city;
+ }
+ public void setCity(String city){
+  this.city=city;
+ }
+ public String getState(){
+  return state;
+ }
+ public void setState(String state){
+  this.state=state;
+ }
+ public String getZipCode(){
+  return zipCode;
+ }
+ public void setZipCode(String zipCode){
+  this.zipCode=zipCode;
+ }
 }
