46cddaa60797d38a156cd3385bac600a570e11cd
Durga Prasad <119026227+durgaaprasadch@users.noreply.github.com>
2026-02-23 00:56:24 +0530

    Reformat Product class with consistent spacing

--- assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Product.java
+++ assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Product.java
@@ -1,19 +1,40 @@
 package com.lpu.Entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 @Entity
 public class Product {
 
- @Id @GeneratedValue
- private Long id;
- private String name;
- private double price;
+    @Id
+    @GeneratedValue
+    private Long id;
 
- public Long getId(){return id;}
- public void setId(Long id){this.id=id;}
- public String getName(){return name;}
- public void setName(String name){this.name=name;}
- public double getPrice(){return price;}
- public void setPrice(double price){this.price=price;}
+    private String name;
+    private double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
