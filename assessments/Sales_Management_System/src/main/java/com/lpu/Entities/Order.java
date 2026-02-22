77ec4a7dfc7baa44d85f303e86f36a5a4c566b38
Durga Prasad <119026227+durgaaprasadch@users.noreply.github.com>
2026-02-23 00:54:38 +0530

    Update Order.java

--- assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Order.java
+++ assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Order.java
@@ -1,33 +1,64 @@
 package com.lpu.Entities;
 
 import jakarta.persistence.*;
-import java.util.*;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Order {
 
- @Id @GeneratedValue
- private Long id;
+    @Id
+    @GeneratedValue
+    private Long id;
 
- @Temporal(TemporalType.DATE)
- private Date orderDate;
+    @Temporal(TemporalType.DATE)
+    private Date orderDate;
 
- private double totalAmount;
+    private double totalAmount;
 
- @ManyToOne
- private Customer customer;
+    @ManyToOne
+    private Customer customer;
 
- @ManyToMany
- private List<Product> products;
+    @ManyToMany
+    private List<Product> products;
 
- public Long getId(){return id;}
- public void setId(Long id){this.id=id;}
- public Date getOrderDate(){return orderDate;}
- public void setOrderDate(Date orderDate){this.orderDate=orderDate;}
- public double getTotalAmount(){return totalAmount;}
- public void setTotalAmount(double totalAmount){this.totalAmount=totalAmount;}
- public Customer getCustomer(){return customer;}
- public void setCustomer(Customer customer){this.customer=customer;}
- public List<Product> getProducts(){return products;}
- public void setProducts(List<Product> products){this.products=products;}
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
 }
