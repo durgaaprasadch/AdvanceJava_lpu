71323bee15f1fdcfe385b78a77231a3a9b321c14
Durga Prasad <119026227+durgaaprasadch@users.noreply.github.com>
2026-02-23 00:52:51 +0530

    Refactor Lead class for improved readability

--- assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Lead.java
+++ assessments/Sales_Management_System/src/main/java/com/lpu/Entities/Lead.java
@@ -5,23 +5,54 @@ import jakarta.persistence.*;
 @Entity
 public class Lead {
 
- @Id @GeneratedValue
- private Long id;
- private String name;
- private String source;
- private String contactInfo;
+    @Id
+    @GeneratedValue
+    private Long id;
 
- @ManyToOne
- private SalesEmployee employee;
+    private String name;
+    private String source;
+    private String contactInfo;
 
- public Long getId(){return id;}
- public void setId(Long id){this.id=id;}
- public String getName(){return name;}
- public void setName(String name){this.name=name;}
- public String getSource(){return source;}
- public void setSource(String source){this.source=source;}
- public String getContactInfo(){return contactInfo;}
- public void setContactInfo(String contactInfo){this.contactInfo=contactInfo;}
- public SalesEmployee getEmployee(){return employee;}
- public void setEmployee(SalesEmployee employee){this.employee=employee;}
+    @ManyToOne
+    private SalesEmployee employee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getContactInfo() {
+        return contactInfo;
+    }
+
+    public void setContactInfo(String contactInfo) {
+        this.contactInfo = contactInfo;
+    }
+
+    public SalesEmployee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(SalesEmployee employee) {
+        this.employee = employee;
+    }
 }
