c0177820c7f6947195f9b98dee05ed87ac705bdb
Durga Prasad <119026227+durgaaprasadch@users.noreply.github.com>
2026-02-23 00:57:22 +0530

    Refactor SalesEmployee class for consistency

--- assessments/Sales_Management_System/src/main/java/com/lpu/Entities/SalesEmployee.java
+++ assessments/Sales_Management_System/src/main/java/com/lpu/Entities/SalesEmployee.java
@@ -6,20 +6,45 @@ import java.util.List;
 @Entity
 public class SalesEmployee {
 
- @Id @GeneratedValue
- private Long id;
- private String name;
- private String department;
+    @Id
+    @GeneratedValue
+    private Long id;
 
- @OneToMany
- private List<Lead> leads;
+    private String name;
+    private String department;
 
- public Long getId(){return id;}
- public void setId(Long id){this.id=id;}
- public String getName(){return name;}
- public void setName(String name){this.name=name;}
- public String getDepartment(){return department;}
- public void setDepartment(String department){this.department=department;}
- public List<Lead> getLeads(){return leads;}
- public void setLeads(List<Lead> leads){this.leads=leads;}
+    @OneToMany
+    private List<Lead> leads;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public List<Lead> getLeads() {
+        return leads;
+    }
+
+    public void setLeads(List<Lead> leads) {
+        this.leads = leads;
+    }
 }
